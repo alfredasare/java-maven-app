54bc315769dc82ee83d770967b1a2d387c78d888
alfredasare <alfredasare101@gmail.com>
2023-03-09 20:50:31 +0000

    Configure version increment in build

--- script.groovy
+++ script.groovy
@@ -1,19 +1,19 @@
 def buildJar() {
     echo "building the application..."
-    sh 'mvn package'
+    sh 'mvn clean package'
 } 
 
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t alfredasare/devops-demo-app:jma-2.0 .'
+        sh "docker build -t alfredasare/devops-demo-app:${IMAGE_NAME} ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push alfredasare/devops-demo-app:jma-2.0'
+        sh "docker push alfredasare/devops-demo-app:${IMAGE_NAME}"
     }
 }
 
 def deployApp() {
-    echo "deploying the application"
+    echo "deploying the application to EC2"
 } 
 
 return this
